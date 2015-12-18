b9f83594a71dd5954d120d8528e8dc259931dd4d
Steven Li <sli@bleacherreport.com>
2015-12-18 12:56:03 -0800

    create model layer

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+#  namespace :api do
+#    resources :todos do, :only => [:create, :index, :show]
+#      resources :comments, :only => [:index] 
+#    end
+#  end
+#
+#  resources :comments, :only => [:create, :show]
+end
+
+
