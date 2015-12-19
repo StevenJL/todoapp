2ba82fa63738b1649bcaac216b7d718a311eb3f0
Steven Li <sli@bleacherreport.com>
2015-12-19 13:33:43 -0800

    model controller tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-#  namespace :api do
-#    resources :todos do, :only => [:create, :index, :show]
-#      resources :comments, :only => [:index] 
-#    end
-#  end
-#
-#  resources :comments, :only => [:create, :show]
-end
+  namespace :api do
+    resources :todos, :only => [:create, :index, :show] do
+      resources :comments, :only => [:index] 
+    end
 
+    resources :comments, :only => [:create, :show]
+  end
+end
 
